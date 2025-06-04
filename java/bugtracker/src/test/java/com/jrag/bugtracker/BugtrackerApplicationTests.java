77b536cae00243008bf5ceb1f773deef046d4b0e
Josh Ragland <59399189+jragland3@users.noreply.github.com>
2025-06-04 15:55:53 -0500

    Add primary java code

---
+++ java/bugtracker/src/test/java/com/jrag/bugtracker/BugtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrag.bugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
